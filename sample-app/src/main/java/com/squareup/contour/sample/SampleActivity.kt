a799a530d45420cd57a13b1908fdf010c576a7f2
John Rodriguez <jrod@squareup.com>
2019-11-01 10:07:59 -0400

    Fix lint failures by migrating to AndroidX

--- sample-app/src/main/java/com/squareup/contour/sample/SampleActivity.kt
+++ sample-app/src/main/java/com/squareup/contour/sample/SampleActivity.kt
@@ -1,8 +1,8 @@
 package com.squareup.contour.sample
 
 import android.os.Bundle
-import android.support.v7.app.AppCompatActivity
 import android.view.View
+import androidx.appcompat.app.AppCompatActivity
 import kotlin.LazyThreadSafetyMode.NONE
 import kotlin.contracts.ExperimentalContracts
 
