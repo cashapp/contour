b05e88f5ad057046dc159758d73d83d5553d657f
John Rodriguez <jrod@squareup.com>
2021-08-08 20:21:57 -0400

    Update lint issue registration mechanism

--- contour-lint/src/main/java/com/squareup/contour/lint/ContourIssueRegistry.kt
+++ contour-lint/src/main/java/com/squareup/contour/lint/ContourIssueRegistry.kt
@@ -17,14 +17,26 @@
 package com.squareup.contour.lint
 
 import com.android.tools.lint.client.api.IssueRegistry
+import com.android.tools.lint.client.api.Vendor
 import com.android.tools.lint.detector.api.CURRENT_API
-import com.android.tools.lint.detector.api.Issue
+import com.google.auto.service.AutoService
 
 @Suppress("UnstableApiUsage", "unused")
+@AutoService(value = [IssueRegistry::class])
 class ContourIssueRegistry : IssueRegistry() {
-  override val issues: List<Issue>
-    get() = listOf(NestedContourLayoutsDetector.ISSUE)
+  override val issues = listOf(NestedContourLayoutsDetector.ISSUE)
 
-  override val api: Int
-    get() = CURRENT_API
+  override val api = CURRENT_API
+
+  /**
+   * works with Studio 4.0 or later; see
+   * [com.android.tools.lint.detector.api.describeApi]
+   */
+  override val minApi = 7
+
+  override val vendor = Vendor(
+    vendorName = "cashapp/contour",
+    identifier = "app.cash.contour:contour:{version}",
+    feedbackUrl = "https://github.com/cashapp/contour/issues",
+  )
 }
